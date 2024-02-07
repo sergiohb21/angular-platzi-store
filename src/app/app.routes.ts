ce7f7edaa0c9e5b3020a1a7c88ae9a6721209a89
[sergioshb21Clase] <[sergiorandy10@gmail.com]>
2024-02-07 15:00:00 +0100

    ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes,RouterModule } from '@angular/router';
 import { TableComponent } from './components/product-table/product-table.component';
-import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { IndexComponent } from './components/index/index.component';
 
 export const routes: Routes = [
-    { path: '', component:ProductDetailComponent  },
+    { path: '', component:IndexComponent  },
     { path: 'tabla', component:TableComponent  },
 ];
--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { ProductService } from '../../services/get-products.service';
 
 @Component({
   selector: 'app-index',
@@ -8,6 +9,19 @@ import { ProductDetailComponent } from '../product-detail/product-detail.compone
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
 
+  constructor(private productService: ProductService) { }
+  
+  ngOnInit () {
+    const Productos = this.loadProducts();
+  }
+
+  loadProducts() {
+    this.productService.getProducts()
+      .then(products => {
+        return products;
+      })
+      .catch(error => console.error(error));
+  }
 }
