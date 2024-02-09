7889b6d5877b9feeacdff4fe026952588da7a99a
[sergioshb21Clase] <[sergiorandy10@gmail.com]>
2024-02-09 14:06:01 +0100

    ok

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,25 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { ProductService } from '../../services/get-products.service';
-
+import { Product } from '../../models/product.model';
+import { MatCardModule } from '@angular/material/card';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [ProductDetailComponent],
+  imports: [ProductDetailComponent,NgIf,MatCardModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
 export class IndexComponent implements OnInit {
+  Products?:Product[];
 
   constructor(private productService: ProductService) { }
   
   ngOnInit () {
-    const Productos = this.loadProducts();
+    this.loadProducts();
   }
 
   loadProducts() {
     this.productService.getProducts()
-      .then(products => {
+      .then((products) => {
+        this.Products = products;
         return products;
       })
       .catch(error => console.error(error));
